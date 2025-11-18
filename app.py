bfa6448e97af6f2e2e0633e168c1385640994af4
Nguyễn Công Cường <148767872+congcuongca1207@users.noreply.github.com>
2025-11-18 07:51:33 +0000

    update web

--- app.py
+++ app.py
@@ -6,43 +6,32 @@ from tensorflow.keras.preprocessing import image
 
 app = Flask(__name__)
 
-# Load model
 model = load_model("garbage_effnetb0.h5")
 
-# Danh sách lớp (chỉnh theo model của bạn)
 class_names = [
     "battery", "biological", "brown-glass", "cardboard",
     "clothes", "green-glass", "metal", "paper",
     "plastic", "shoes", "trash", "white-glass"
 ]
 
-@app.route('/')
-def home():
+@app.route("/")
+def index():
     return render_template("index.html")
 
-
-@app.route('/predict', methods=["POST"])
+@app.route("/predict", methods=["POST"])
 def predict():
-    if 'image' not in request.files:
-        return render_template("index.html", prediction="No file uploaded")
+    file = request.files["image"]
+    path = "static/upload.jpg"
+    file.save(path)
 
-    file = request.files['image']
+    img = image.load_img(path, target_size=(160,160))
+    arr = image.img_to_array(img)
+    arr = np.expand_dims(arr, axis=0)/255.0
 
-    file_path = "static/uploaded.jpg"
-    file.save(file_path)
+    pred = model.predict(arr)
+    class_name = class_names[np.argmax(pred)]
 
-    # Preprocess ảnh
-    img = image.load_img(file_path, target_size=(160, 160))   # đúng input model
-    img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0) / 255.0
-
-    # Dự đoán
-    prediction = model.predict(img_array)
-    class_id = np.argmax(prediction)
-    class_name = class_names[class_id]
-
-    return render_template("index.html", prediction=class_name, image_path=file_path)
-
+    return render_template("index.html", prediction=class_name)
 
 if __name__ == "__main__":
     app.run(debug=True)
